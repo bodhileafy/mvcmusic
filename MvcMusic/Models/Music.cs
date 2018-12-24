1e770be5b9f98990e9883fe7152c7c1d27cc448d
Tan Chin Leong <cltan8@gmail.com>
2018-12-24 14:44:52 +0800

    Base project.

---
+++ MvcMusic/Models/Music.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace MvcMusic.Models
+{
+    public class Music
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class MusicDBContext : DbContext
+    {
+        public DbSet<Music> Musics { get; set; }
+    }
+}
